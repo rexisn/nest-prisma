0c75543c56f7e3d860b2e0a723d7b98a25525025
rexisn <ndizihiweregis06@gmail.com>
2022-07-06 21:35:21 +0200

    Minor changes : controller class definitions

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor (private AuthService : AuthService) {
+        this.AuthService = AuthService
+    }
+
+    @Post("/user/signup")
+    signup(){
+
+    }
+
+    
+    @Post("/user/login")
+    login(){
+        
+    }
+
+    @Post("/logout")
+    logout(){
+
+    }
+
+    @Post("/refreshtoken")
+    refresh(){
+        
+    }
+}
